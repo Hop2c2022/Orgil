9004ec15a3e3670483acfaf3e20b25c04790c805
2022Hop2c-Orgil <orgil@iMac2021-08s-iMac.local>
2022-12-23 16:19:28 +0800

    all

---
+++ express/data.js
@@ -0,0 +1,22 @@
+export const Database = [
+  {
+    id: "101",
+    name: "orgil",
+    job: "student",
+  },
+  {
+    id: "102",
+    name: "boldo",
+    job: "student",
+  },
+  {
+    id: "103",
+    name: "bata",
+    job: "niseh",
+  },
+  {
+    id: "104",
+    name: "dorjo",
+    job: "unah",
+  },
+];
